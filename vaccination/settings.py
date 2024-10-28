76c47605d958100f80dd63e18b12c8dd18e60b7c
SreayaRajan <sreayarajan75@gmail.com>
2024-10-28 15:37:15 +0530

    email code

--- vaccination/settings.py
+++ vaccination/settings.py
@@ -158,5 +158,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com' 
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'sumishasudha392@gmail.com'
-EMAIL_HOST_PASSWORD = 'ebvd zdgw thrs sgsf'
+EMAIL_HOST_USER = 'sreayarajan75@gmail.com'
+EMAIL_HOST_PASSWORD = 'kouy inwf pahj vejt'
